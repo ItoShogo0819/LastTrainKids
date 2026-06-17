9e20e956f0535079bc2cad7fd9d8a6a606dd0b5b
flaming8634 <flaming.8634@icloud.com>
2026-06-17 10:54:03 +0900

    Reapply "[Add]InputReaderのスクリプトを生成"
    This reverts commit 07fd2b3f7ea84a004489df9014db1b7100ec5e2d.

---
+++ Assets/Player/PlayerScripts/InputReader.cs
@@ -0,0 +1,52 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputReader : MonoBehaviour
+{
+    public event Action<float> SteerChanged;
+    public event Action<bool> AccelerateChanged;
+    public event Action<bool> BrakeChanged;
+    public event Action HornPressed;
+    public event Action RetryPressed;
+    public event Action PausePressed;
+
+    public void OnSteer(InputAction.CallbackContext cont)
+    {
+        SteerChanged?.Invoke(cont.ReadValue<float>());
+    }
+
+    public void OnAccelerate(InputAction.CallbackContext cont)
+    {
+        AccelerateChanged?.Invoke(cont.ReadValueAsButton());
+    }
+
+    public void OnBrake(InputAction.CallbackContext cont)
+    {
+        BrakeChanged?.Invoke(cont.ReadValueAsButton());
+    }
+
+    public void OnHorn(InputAction.CallbackContext cont)
+    {
+        if (cont.performed)
+        {
+            HornPressed?.Invoke();
+        }
+    }
+
+    public void OnRetry(InputAction.CallbackContext cont)
+    {
+        if (cont.performed)
+        {
+            RetryPressed?.Invoke();
+        }
+    }
+
+    public void OnPause(InputAction.CallbackContext cont)
+    {
+        if (cont.performed)
+        {
+            PausePressed?.Invoke();
+        }
+    }
+}
